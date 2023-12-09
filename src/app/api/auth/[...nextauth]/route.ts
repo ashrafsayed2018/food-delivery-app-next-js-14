01ebf4b0e1df154334bce447e3e6c04808105f9a
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2023-12-10 00:46:41 +0200

    modifying route.ts in auth dir by adding handler and exporting as POST and GET

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
-export default NextAuth({
+const handler = NextAuth({
     providers: [
 
         CredentialsProvider({
@@ -40,3 +40,5 @@ export default NextAuth({
         
     ],
 })
+
+export { handler as GET, handler as POST }
